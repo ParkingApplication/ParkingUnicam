8ff696ee77d784ec0ee21d5488a3ee6d0a4c8421
Unknown <lorenzo.stacchio@studenti.unicam.it>
2018-01-09 22:23:57 +0100

    Modifiche file login
    Inserimento di salvataggio dati quando utente logga.

--- Client/app/src/main/java/com/example/stach/app_test/LoginActivity.java
+++ Client/app/src/main/java/com/example/stach/app_test/LoginActivity.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.StringTokenizer;
 
 public class LoginActivity extends AppCompatActivity {
+    //text file for save login in local file
+    File login_file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,45 +23,79 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = getIntent();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.recruitData();
+    }
+
     /**
      * This method will send data to server to verify user credentials.
      */
-    public void sendDataForLogin(View view){
-        EditText mail =(EditText) findViewById(R.id.mail);
-        EditText password =(EditText) findViewById(R.id.pass);
+    public void sendDataForLogin(View view) {
+        EditText mail = (EditText) findViewById(R.id.mail);
+        EditText password = (EditText) findViewById(R.id.pass);
         //contact server
+        //IF SERVER TOLD THAT LOG STATS ARE OK, SAVE DATA IN A LOCAL TXT FILE FOR NEXT LOGINS
         //save data
-        this.saveData(mail.toString(),password.toString());
+        this.saveData(mail.getText().toString(), password.getText().toString());
     }
 
     /**
      * This method is use to save user data after login, to allow user to overdrop the login activity if is logged yet.
      */
-    public void saveData(String mail, String password){
-        //Create file for save login
-        File login_file = new File(this.getFilesDir(), "LoginStats.txt");
+    public void saveData(String mail, String password) {
+       login_file = new File(this.getFilesDir(), "LoginStats.txt");
+        //try to write in file
         try {
-            BufferedWriter fos = new BufferedWriter(new FileWriter(login_file.getAbsolutePath() +"/"+"LoginStats.txt"));
-            fos.write(mail.trim());
-            fos.write(password.trim());
+            BufferedWriter fos = new BufferedWriter(new FileWriter(login_file.getAbsolutePath()));
+            fos.write(mail + " ");
+            fos.write(password + " ");
             fos.close();
-            Toast.makeText(this, "Saved", Toast.LENGTH_LONG);
         } catch (Exception e) {
-            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG);
+            e.printStackTrace();
         }
     }
 
+    /**
+     * This method is use to recruit user data before login, to allow user to overdrop the login activity if is logged yet.
+     */
+    public void recruitData() {
+        EditText mail = (EditText) findViewById(R.id.mail);
+        EditText pass = (EditText) findViewById(R.id.pass);
+        //try to read in file
+        try {
+            String appoggio;
+            BufferedReader fos1 = new BufferedReader(new FileReader(login_file.getAbsolutePath()));
+            StringBuilder sb = new StringBuilder();
+            while ((appoggio = fos1.readLine()) != null) {
+                sb.append(appoggio);
+            }
+            fos1.close();
+            //tokenize string builder
+            StringTokenizer sT = new StringTokenizer(sb.toString());
+            //assignment
+            String mailResult = sT.nextToken();
+            String passwordResult = sT.nextToken();
+            //put result
+            mail.setText(mailResult);
+            pass.setText(passwordResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     /**
      * This method allow user to go signUp activity
      */
-    public void goToSignUp(View view){
+    public void goToSignUp(View view) {
         startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
     }
+
     /**
      * This method allow user to go gps activity
      */
-    public void goToGps(View view){
+    public void goToGps(View view) {
         startActivity(new Intent(LoginActivity.this, GPSActivity.class));
     }
 

086129402cfa8a38ab24d74d0bdc96344e43f2d3
Unknown <lorenzo.stacchio@studenti.unicam.it>
2018-03-05 11:18:59 +0100

    Insert Place Picker + Modifiche al codice varie

---
+++ Client/app/src/main/java/com/example/stach/app_test/Automatic_Search_Fragment.java
@@ -0,0 +1,31 @@
+package com.example.stach.app_test;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class Automatic_Search_Fragment extends Fragment {
+
+
+    public Automatic_Search_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_automatic__search_, container, false);
+    }
+
+}
---
+++ Client/app/src/main/java/com/example/stach/app_test/Input_Search_Fragment.java
@@ -0,0 +1,31 @@
+package com.example.stach.app_test;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class Input_Search_Fragment extends Fragment {
+
+
+    public Input_Search_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input__search_, container, false);
+    }
+
+}

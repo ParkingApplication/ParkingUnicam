e6f358150871c00dc781e1dd4fac4b152c027954
Unknown <lorenzo.stacchio@studenti.unicam.it>
2018-03-19 12:33:28 +0100

    Prima scrittura contatto server per trovare parcheggi in città
    TO TEST

--- Client/app/src/main/java/com/example/stach/app_test/chooseParkingFragment.java
+++ Client/app/src/main/java/com/example/stach/app_test/chooseParkingFragment.java
@@ -1,6 +1,7 @@
 package com.example.stach.app_test;
 
 
+import android.app.ProgressDialog;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -8,9 +9,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Locale;
 
@@ -20,6 +26,7 @@ import java.util.Locale;
  */
 public class chooseParkingFragment extends Fragment {
     private View view;
+    static ProgressDialog caricamento = null;
 
     public chooseParkingFragment() {
         // Required empty public constructor
@@ -38,12 +45,11 @@ public class chooseParkingFragment extends Fragment {
         String longitudine = bundle.getString("longitudine");
         //ASSEGNO I VALORI ALLE TEXT VIEW
         TextView località = (TextView) view.findViewById(R.id.resultFromGPS);
-        località.setText(latitudine+" "+ longitudine);
-        getCityFromLatLong(latitudine,longitudine);
+        località.setText(getCityFromLatLong(latitudine,longitudine));
         return view;
     }
 
-    private void getCityFromLatLong(String lat, String lng){
+    private String getCityFromLatLong(String lat, String lng){
         Geocoder gcd = new Geocoder(this.getContext(), Locale.getDefault());
         List<Address> addresses = null;
         try {
@@ -52,11 +58,35 @@ public class chooseParkingFragment extends Fragment {
             e.printStackTrace();
         }
         if (addresses.size() > 0) {
-            TextView località = (TextView) view.findViewById(R.id.resultFromGPS);
-            località.setText(addresses.get(0).getLocality());
+            return addresses.get(0).getLocality();
         }
         else {
-            // do your stuff
+           System.out.print("Non ci sono parametri validi per trovare la città");
+           return "No city";
         }
+    }
+
+    /**Method to call server
+     * for get parking in certain citY
+     */
+    public void sendDataForViewPark() {
+
+        TextView località = (TextView) view.findViewById(R.id.resultFromGPS);
+        // Prelevo i dati per il login per inviarli al server.
+        String citta = località.getText().toString();
+        // Inserisco i dati del login in un HashMap così da poterli convertire facilmente in JSonObject in seguito
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("città", citta);
+        }catch (Exception e){}
+
+        // Avverto l'utente del tentativo di invio dei dati di login al server
+        caricamento = ProgressDialog.show(this.getActivity(), "",
+                "Connessione con il server in corso...", true);
+
+        // Creo ed eseguo una connessione con il server web
+        Connessione conn = new Connessione(postData, "POST",this.getContext(),this.getActivity(), null);
+        conn.execute(Parametri.IP + "/login");
+
     }
 }

37f4dfa233f07dc107bcccfe963c42b4bc74999e
Unknown <matteo.lupini@studenti.unicam.it>
2018-03-19 17:50:33 +0100

    Server Parcheggi
    Gestione Parcheggio sbarra

---
+++ ParkingUnicamServerParcheggi/connectionToNode.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.*;
+
+public class connectionToNode {
+	
+	 HttpURLConnection conn = null;
+	 String url = " ";
+	 
+	 
+public connectionToNode(String stringaUrl) {
+	this.url = stringaUrl;
+}
+
+
+//Funzione del server che controlla i QR code in entrata e chiede l'autenticazione al server
+public String inviaQRCode(String qrCode, String path)  {	
+	String result = " ";
+	String urlPerConnessione = url;
+	
+	try {
+	 URL url = new URL(urlPerConnessione + path);
+     conn = (HttpURLConnection)url.openConnection();
+
+     if ( conn != null ) {                 
+         String strPostData = qrCode;
+         JSONObject jsonObj = new JSONObject();
+         jsonObj.put("QRCODE", strPostData);
+
+         conn.setDoOutput(true);
+         conn.setDoInput(true);
+         conn.setUseCaches(false);
+         conn.setRequestProperty( "Content-Type", "application/json" );
+         conn.setRequestProperty("Accept", "application/json");
+         conn.setRequestMethod("POST");
+         
+         
+         DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+         dos.writeBytes(jsonObj.toString());
+         dos.close();
+         
+         int codice = conn.getResponseCode(); 
+         
+         
+         //controlla il codice nel caso di successo
+         if(codice == 200) {
+        	 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+             StringBuilder sb = new StringBuilder();
+             String line;
+             while ((line = br.readLine()) != null) {
+                 sb.append(line+"\n");
+             }
+             br.close();
+             result = sb.toString();
+             result = estraiSuccessful(result);
+         	}
+         
+         
+         //controlla il codice nel caso di fallimento
+         if (codice == 237) {
+        	 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+             StringBuilder sb = new StringBuilder();
+             String line;
+             while ((line = br.readLine()) != null) {
+                 sb.append(line+"\n");
+             }
+             br.close();
+             result = sb.toString(); 
+             result = estraiErrore(result);
+         	}
+         
+         conn.disconnect();
+     		}
+		}
+		catch(Exception e){
+		System.out.println("PIPPO");
+	}
+	
+	return result;
+}
+
+
+
+
+// Funzione per l'estrazione automatica del dato JSon error
+private String estraiErrore(String data){
+    String result = "";
+
+    try {
+        JSONObject response = new JSONObject(data);
+        JSONObject error = new JSONObject(response.getString("error"));
+        result = error.getString("info");
+    } catch (Exception e) {
+        result = "Impossibile leggere la risposta del server.";
+    }
+
+    return result;
+}
+
+// Funzione per l'estrazione automatica del dato JSon successful
+private String estraiSuccessful(String data){
+    String result = "";
+
+    try {
+        JSONObject response = new JSONObject(data);
+        JSONObject error = new JSONObject(response.get("succesful"));
+        result = (String) error.get("info");
+    } catch (Exception e) {
+        result = "Impossibile leggere la risposta del server.";
+    }
+
+    return result;
+}
+}
---
+++ ParkingUnicamServerParcheggi/serverParcheggio.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class serverParcheggio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		connectionToNode cToNode = new connectionToNode("http://2.226.207.189");
+		String rispostaPerTest = " ";
+		while(true) {
+			System.out.println("Inserire Il Numero Relativo all'azione da Compiere");
+			System.out.println("1 : Entrata");
+			System.out.println("2 : Uscita");
+			
+			
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				
+				String line = buffer.readLine();
+				
+				System.out.println("Inserire il QRCode");
+				BufferedReader bufferQRCode = new BufferedReader(new InputStreamReader(System.in));	
+				String lineQRCode = bufferQRCode.readLine();
+				
+				
+				char level = line.charAt(0);
+				 
+				switch(level) {
+		         case '1' :
+		            System.out.println("Enter the parking"); 
+		            rispostaPerTest = cToNode.inviaQRCode(lineQRCode, "/parcheggio/entrataAutomobilista");
+		            break;
+		         case '2' :
+		            System.out.println("Wait while exit");
+		            rispostaPerTest = cToNode.inviaQRCode(lineQRCode, "/parcheggio/uscitaAutomobilista");
+		            break;
+		         default :
+		            System.out.println("Invalid Input");
+		      }
+		      System.out.println("Your solution is: " + level);
+		   }
+			
+			catch(IOException e) { 
+					System.out.println(e.getMessage());
+		}
+
+		     System.out.println(rispostaPerTest);
+			
+	}
+	
+	}
+}
